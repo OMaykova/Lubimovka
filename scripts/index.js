234f2e8b990958fee8e25431ad5e355ec2152f73
Olga Maykova <dolkaaaaa@gmail.com>
2022-06-05 00:22:38 +0300

    feat html, css

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
+const swiper = new Swiper('.reviews-slider', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
