6242c7c8c678891ef88f770c35026a91139f6c51
Olga Maykova <dolkaaaaa@gmail.com>
2022-06-06 00:56:48 +0300

    feat slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,31 @@
-const swiper = new Swiper('.reviews-slider', {
-  // If we need pagination
+const swiper = new Swiper('.swiper'
+, {
+  loop: true,
+  slidesPerView: 3,
+    // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true
+    el: '.slider-pagination',
+    clickable: true,
   },
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    prevEl: '.slider-button-prev',
+    // nextEl: value.nextElementSibling.nextElementSibling,
+    // prevEl: value.nextElementSibling,
   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  watchOverflow: true,
+  spaceBetween: 30,
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView:1
+  //   }
+  // },
+
 });
