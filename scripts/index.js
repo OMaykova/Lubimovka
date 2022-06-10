234823f8e3b5ca80d68c38b2862aadceb450148c
Apple <IIKolchin@yandex.ru>
2022-06-10 23:45:41 +0300

    fix remarks

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import { initialPhotos, createPhoto } from './utils.js'
+
 const swiperPhoto = new Swiper(".mySwiper", {
   slidesPerView: 4,
   grid: {
@@ -6,53 +8,8 @@ const swiperPhoto = new Swiper(".mySwiper", {
   spaceBetween: 1,
 });
 
-const initialPhotos = [
-  {
-    src: "./images/image1.png",
-  },
-  {
-    src: "./images/image2.png",
-  },
-  {
-    src: "./images/image3.png",
-  },
-  {
-    src: "./images/image4.png",
-  },
-  {
-    src: "./images/image5.png",
-  },
-  {
-    src: "./images/image6.png",
-  },
-  {
-    src: "./images/image7.png",
-  },
-  {
-    src: "./images/image8.png",
-  },
-  {
-    src: "./images/image1.png",
-  },
-  {
-    src: "./images/image2.png",
-  },
-  {
-    src: "./images/image3.png",
-  },
-];
-
+console.log(initialPhotos)
 const elementContainer = document.querySelector(".photo__swiper-wrapper");
-
-const createPhoto = (img) => {
-  const photoTemplate = document.querySelector(".photo-template").content;
-  const photoElement = photoTemplate
-    .querySelector(".photo__swiper-slide")
-    .cloneNode(true);
-  photoElement.src = img;
-  photoElement.alt = "Фотография из пьессы.";
-  return photoElement;
-};
 
 initialPhotos.forEach((el) => {
   const photo = createPhoto(el.src);
