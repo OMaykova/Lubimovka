180e47b9b7687166d79249a652fe8119be401aef
Alex Voitekhovich <a.voitekhovich@gmail.com>
2022-06-19 14:12:14 +0300

    fix: Адаптивный header

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
+const headerMenu = document.querySelector('.menu');
+const buttonBurger = document.querySelector('.menu__burger');
 
+const burgerMenuActive = () => {
+  headerMenu.classList.toggle('menu_active');
+}
+
+buttonBurger.addEventListener('click', burgerMenuActive);
