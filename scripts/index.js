6b5be28e805beb21b05e0df056597d72852e0c05
Olga Maykova <dolkaaaaa@gmail.com>
2022-06-20 21:44:05 +0300

    merge

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,39 @@ initialPhotos.forEach((el) => {
   const photo = createPhoto(el.src);
   elementContainer.append(photo);
 });
+
+
+// =======feat/slider-reviews
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 3,
+
+  pagination: {
+    el: '.slider-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  watchOverflow: true,
+  spaceBetween: 30,
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1440: {
+      slidesPerView: 3
+    }
+  },
+});
+// >>>>>>> feat/slider-reviews
