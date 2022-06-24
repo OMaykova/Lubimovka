394ed81969009f9dd8eba7cf065171352001da70
Apple <IIKolchin@yandex.ru>
2022-06-24 21:05:44 +0300

    create breakpoints

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,28 @@ const swiperPhoto = new Swiper(".mySwiper", {
     rows: 2,
   },
   spaceBetween: 1,
+  breakpoints: {
+    1440: {
+      slidesPerView: 4,
+      grid: {
+        rows: 2,
+      },
+    },
+    1200: {
+      slidesPerView: 3,
+      grid: {
+        rows: 3,
+      },
+      spaceBetween: 1,
+    },
+    300: {
+      slidesPerView: 2,
+      grid: {
+        rows: 4,
+      },
+      spaceBetween: 1,
+    },
+  }
 });
 
 const elementContainer = document.querySelector(".photo__swiper-wrapper");
